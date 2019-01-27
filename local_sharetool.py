005086d81ed303133182b60a0f5ca519399f01d7
Michael Guffre <mtguffr@gmail.com>
2019-01-26 19:42:29 -0500

    Got rid of pywin32 dependency

--- local_sharetool.py
+++ local_sharetool.py
@@ -3,14 +3,33 @@ import ctypes
 import socket
 import webbrowser
 from threading import Thread
-
-# Encryption imports
-import nacl.secret
 import hashlib
 
 # 3rd party imports
+import nacl.secret
 import win10toast as toast
-import win32clipboard as clip
+
+# Clipboard globals
+CF_TEXT = 1
+kernel32 = ctypes.windll.kernel32
+user32 = ctypes.windll.user32
+kernel32.GlobalLock.argtypes = [ctypes.c_void_p]
+kernel32.GlobalLock.restype = ctypes.c_void_p
+kernel32.GlobalUnlock.argtypes = [ctypes.c_void_p]
+user32.GetClipboardData.restype = ctypes.c_void_p
+
+def get_clipboard_text():
+    user32.OpenClipboard(0)
+    try:
+        if user32.IsClipboardFormatAvailable(CF_TEXT):
+            data = user32.GetClipboardData(CF_TEXT)
+            data_locked = kernel32.GlobalLock(data)
+            text = ctypes.c_char_p(data_locked)
+            value = text.value
+            kernel32.GlobalUnlock(data_locked)
+            return value
+    finally:
+        user32.CloseClipboard()
 
 class Comm(object):
     def __init__(self, port, pword):
@@ -51,9 +70,7 @@ class Comm(object):
     
     def _send_loop_thread(self, hist=""):
         while True:
-            clip.OpenClipboard()
-            data = clip.GetClipboardData()
-            clip.CloseClipboard()
+            data = get_clipboard_text()
             if self.data_check(data) and hist != data:
                 confirm = ctypes.windll.user32.MessageBoxA(0, str(data), "Share to network?", 0x1)
                 if confirm == 1:
